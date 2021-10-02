0119d8fcd8d6e232bd1eae8861230420d8be0283
Aditya Saxena <aditya.saxena33@gmail.com>
2021-10-02 14:01:07 +0530

    serverprop

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,18 @@ import Link from "next/link";
 import Head from "next/head";
 import Layout from "../components/layout";
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  // const a = fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=XxIWNaawgfJKnli5VTPQdRxcpPNijxn4WRmdLg1D")
+  const a = [{ aditya: 23 }, { saxena: 53 }];
+
+  return {
+    props: {
+      a,
+    },
+  };
+}
+
+export default function Home(props) {
   return (
     <Layout>
       <Head>
@@ -11,7 +22,10 @@ export default function Home() {
       <div className="flexbox">
         <div>
           <div className="heading">Welcome to Atlas AI</div>
-          <div>One Stop Solution for all your writing problems</div>
+          <div style={{ textAlign: "right" }}>
+            Say goodbye to your writing problems
+          </div>
+          <div>{props.a.saxena}</div>
         </div>
 
         <Link href="/admin">Admin Panel</Link>

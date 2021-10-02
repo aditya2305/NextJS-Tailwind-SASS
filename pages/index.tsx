cca89cd84d8ca44319ff49c589950a664550c55a
Aditya Saxena <aditya.saxena33@gmail.com>
2021-10-03 00:48:57 +0530

    fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../components/layout";
+import { GetServerSideProps, GetStaticProps } from "next";
+import Image from "next/image";
 
-export async function getServerSideProps(context) {
-  // const a = fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=XxIWNaawgfJKnli5VTPQdRxcpPNijxn4WRmdLg1D")
-  const a = [{ aditya: 23 }, { saxena: 53 }];
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch(
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=XxIWNaawgfJKnli5VTPQdRxcpPNijxn4WRmdLg1D"
+  );
+
+  const data = res.json();
 
   return {
-    props: {
-      a,
-    },
+    // revalidate: 10, // WILL RERUN GETSTATICPROPS ON SERVER EVERY 10 SECONDS FOR EVERY REQUEST
+    props: { data: await data },
   };
-}
+};
 
-export default function Home(props) {
+export default function Home({ data }) {
   return (
     <Layout>
       <Head>
@@ -25,7 +29,21 @@ export default function Home(props) {
           <div style={{ textAlign: "right" }}>
             Say goodbye to your writing problems
           </div>
-          <div>{props.a.saxena}</div>
+          <br />
+          <div>
+            {data.photos.slice(0, 10).map((item) => {
+              return (
+                <div>
+                  <img src={item.img_src} alt="" />
+                  {/* <Image
+                    loader={() => item.img_src}
+                    src={item.img_src}
+                    alt="Picture of the author"
+                  /> */}
+                </div>
+              );
+            })}
+          </div>
         </div>
 
         <Link href="/admin">Admin Panel</Link>
